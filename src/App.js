1d47d71c6a8324c59bd4eb8a868d84f6afe5a1a1
Dimuthu-Pinsara <kdpjayathilaka@gmail.com>
2025-12-09 17:44:56 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect } from "react";
+import { initTipNixTooltip } from "tipnix";
+import "tipnix/tipnix.css"; 
 
 function App() {
+
+  useEffect(() => {
+    initTipNixTooltip({
+      backgroundColor: "#000000",
+      textColor: "#ffffff",
+      fontSize: "16px",
+      animation: "wobble",
+      width: "225px",
+      padding: "10px",
+      parentWrapElement: ".body"
+    });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div
+        className="tipnix"
+        tipnix-bg="#00aaff"
+        tipnix-text-color="#ffffff"
+        tipnix-font-size="18px"
+        tipnix-text="Tipnix working with the classic Next.js pages router."
+        tipnix-width="200px"
+        tipnix-padding="16px"
+        tipnix-animation="wobble"
+        style={{ marginTop: 30 }}
+      >
+        <span>Hover me (Next Pages)</span>
+      </div>
   );
 }
 
