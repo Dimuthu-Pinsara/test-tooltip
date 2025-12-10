d0a14fab8c3267b8426c52eafa4b51a1d8e6d204
Dimuthu-Pinsara <kdpjayathilaka@gmail.com>
2025-12-10 15:45:21 +0530

    implement Tooltip component

--- src/App.js
+++ src/App.js
@@ -1,36 +1,30 @@
 import './App.css';
+import Tooltip from './components/tooltip';
 import { useEffect } from "react";
-import { initTipNixTooltip } from "tipnix";
-import "tipnix/tipnix.css"; 
 
 function App() {
 
   useEffect(() => {
-    initTipNixTooltip({
-      backgroundColor: "#000000",
-      textColor: "#ffffff",
-      fontSize: "16px",
-      animation: "wobble",
-      width: "225px",
-      padding: "10px",
-      parentWrapElement: ".body"
-    });
+    document.body.setAttribute("dir", "rtl");
   }, []);
 
   return (
-    <div
-        className="tipnix"
-        tipnix-bg="#00aaff"
-        tipnix-text-color="#ffffff"
-        tipnix-font-size="18px"
-        tipnix-text="Tipnix working with the classic Next.js pages router."
-        tipnix-width="200px"
-        tipnix-padding="16px"
-        tipnix-animation="wobble"
-        style={{ marginTop: 30 }}
-      >
-        <span>Hover me (Next Pages)</span>
+    <>
+      <div className='test' style={{ width: "100%", border: "1px solid red" }}>
+        <Tooltip
+          tipnix-bg="#00aaff"
+          tipnix-text-color="#ffffff"
+          tipnix-font-size="12px"
+          tipnix-text="Tipnix working with the classic Next.js pages router."
+          tipnix-width="200px"
+          tipnix-padding="16px"
+          tipnix-animation="wobble"
+          tipnix-parent=".test"
+        >
+          <span>Hover me</span>
+        </Tooltip>
       </div>
+    </>
   );
 }
 
---
+++ src/components/tooltip.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { useEffect } from "react";
+import { initTipNixTooltip } from "tipnix";
+import "tipnix/tipnix.css"; 
+
+export default function Tooltip({...props}) {
+    useEffect(() => {
+        initTipNixTooltip({
+          backgroundColor: "#000000",
+          textColor: "#ffffff",
+          fontSize: "16px",
+          animation: "wobble",
+          width: "225px",
+          padding: "10px",
+          parentWrapElement: ".body"
+        });
+      }, []);
+  return (
+    <div
+        className="tipnix"
+        {...props}
+        style={{ marginTop: 30 }}
+      >
+        <span>Hover me (Next Pages)</span>
+      </div>
+  )
+}
